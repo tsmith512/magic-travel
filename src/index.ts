65fdfa7f8720944a14a7f401c4fd6f3616ae9bd3
Taylor Smith <tsmith@cloudflare.com>
2024-11-13 11:40:03 -0600

    Add Google Maps types and make a very simple API call for initial response

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@
  *
  * Learn more at https://developers.cloudflare.com/workers/
  */
-import { IttyRouter, json, error } from 'itty-router';
+import { IttyRouter, json, error, createResponse } from 'itty-router';
 
 const router = IttyRouter();
 
@@ -26,10 +26,36 @@ router.all('/api/*', ({ query }, env) => {
   }
 });
 
-router.get('/api/directions/:from/:to/', ({ params }) => {
-  return new Response(`Directions from ${params.from} to ${params.to}`);
+router.get('/api/directions/:from/:to/', async ({ params }, env) => {
+  // @TODO: Would it be better to load the GMaps API library here?
+  const directions = await
+    fetch(`https://maps.googleapis.com/maps/api/directions/json?origin=${params.from}&destination=${params.to}&key=${env.GMAPS_API_KEY}&region=us&mode=driving`)
+    .then(res => res.json()) as google.maps.DirectionsResult;
+
+  if (directions.routes.length < 1) {
+    return new Response('Failed to find a driving route', {status: 500});
+  }
+
+  const route = directions?.routes[0];
+  const output = {
+    start: route.legs[0].start_address ?? null,
+    end: route.legs[0].end_address ?? null,
+    duration: route.legs[0].duration?.value ?? null,
+    distance: route.legs[0].distance?.value ?? null,
+    description: route.summary ?? null,
+  };
+
+  return output;
+});
+
+const xml = createResponse('text/xml', (input) => {
+  return [
+    '<root>',
+    ...Object.entries(input).map(([k, v]) => `<${k}>${v}</${k}>`),
+    '</root>',
+  ].join('\n');
 });
 
 export default {
-  fetch: (req:Request, env:Env) => router.fetch(req, env).then(json).catch(error)
+  fetch: (req:Request, env:Env) => router.fetch(req, env).then(xml).catch(error)
 } satisfies ExportedHandler<Env>;
