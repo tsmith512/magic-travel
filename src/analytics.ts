06a1431c8c3330ec1f3f62fe6a6afeda90f42dea
Taylor Smith <tsmith@cloudflare.com>
2024-11-19 13:03:32 -0600

    Clean / comment

--- src/analytics.ts
+++ src/analytics.ts
@@ -5,7 +5,13 @@ type lookupMethod = "API" | "KV" | "CDN";
 
 export const writeAnalyticsEvent = (data: DrivingRouteInfo, request: AuthenticatedRequest, method: lookupMethod, env: Env): void => {
   env.MAGIC_TRAVEL_ANALYTICS.writeDataPoint({
-    'blobs': [data.start, data.end, request.verifiedKey, request.sheetVersion, method],
+    'blobs': [
+      data.start,
+      data.end,
+      request.verifiedKey, // @TODO: This is an index so it doesn't also need to be here?
+      request.sheetVersion,
+      method
+    ],
     'doubles': [data.distance, data.duration],
     'indexes': [(request?.query?.key?.toString() ?? 'unknown')],
   });
